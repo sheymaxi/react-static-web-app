e95b430a52fdfde47bcdd52f09fd1aa69e7b99d9
Seyi Akinjinmi <sakinjinmi@gmail.com>
2024-12-30 18:09:15 +0100

    Update AutoServiceChart.js

--- src/components/AutoServiceChart.js
+++ src/components/AutoServiceChart.js
@@ -1,4 +1,32 @@
+// import React from 'react';
+
+// const AutoServiceChart = ({ data, type, title }) => {
+//   const ChartComponent = () => {
+//     switch (type) {
+//       case 'line':
+//         return <div>Line Chart Placeholder</div>;
+//       case 'bar':
+//         return <div>Bar Chart Placeholder</div>;
+//       case 'pie':
+//         return <div>Pie Chart Placeholder</div>;
+//       default:
+//         return <div>No visualization available</div>;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h3>{title}</h3>
+//       <ChartComponent />
+//     </div>
+//   );
+// };
+
+// export default AutoServiceChart;
+
+
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const AutoServiceChart = ({ data, type, title }) => {
   const ChartComponent = () => {
@@ -15,11 +43,26 @@ const AutoServiceChart = ({ data, type, title }) => {
   };
 
   return (
-    <div>
+    <div style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
       <h3>{title}</h3>
-      <ChartComponent />
+      {data && data.length > 0 ? (
+        <ChartComponent />
+      ) : (
+        <div>No data available for visualization</div>
+      )}
     </div>
   );
+};
+
+AutoServiceChart.propTypes = {
+  data: PropTypes.array.isRequired,
+  type: PropTypes.oneOf(['line', 'bar', 'pie']).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+AutoServiceChart.defaultProps = {
+  data: [],
+  title: 'Default Chart Title',
 };
 
 export default AutoServiceChart;
